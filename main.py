03e4e0ac1eb3970a936799f3b38bd9da9de43c50
asdf <asdfghjkl4775@gmail.com>
2024-08-01 12:39:56 +0900

    ai_recomm 올랜덤

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
 from starlette.responses import HTMLResponse
 import os
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+import uvicorn
+from sklearn.cluster import KMeans
+from PIL import Image
+import numpy as np
+import os
+from random import sample
 
 
 app = FastAPI()
@@ -11,6 +19,41 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+def ensure_directory_exists(path):
+    os.makedirs(path, exist_ok=True)  # exist_ok=True 옵션을 통해 이미 디렉토리가 있으면 에러를 발생시키지 않음
+
+
+@app.post("/upload/")
+async def handle_upload(file: UploadFile = File(...)):
+    upload_dir = "static/uploads"
+    result_dir = "static/results"
+    if not os.path.exists(upload_dir):
+        os.makedirs(upload_dir)
+    if not os.path.exists(result_dir):
+        os.makedirs(result_dir)
+
+    file_location = os.path.join(upload_dir, file.filename)
+    with open(file_location, "wb+") as file_object:
+        file_object.write(await file.read())
+
+    output_path = os.path.join(result_dir, f"clustered_{file.filename}")
+    # Assuming save_clustered_image is defined elsewhere and works correctly
+    save_clustered_image(file_location, output_path)
+
+    return {"info": "Uploaded and Processed", "result_image": f"/static/results/clustered_{file.filename}"}
+
+def save_clustered_image(file_path, output_path):
+    image = Image.open(file_path)
+    data = np.array(image)
+    reshaped_data = data.reshape((-1, 3))
+    kmeans = KMeans(n_clusters=5)
+    kmeans.fit(reshaped_data)
+    clustered_data = kmeans.cluster_centers_[kmeans.labels_]
+    clustered_image = clustered_data.reshape(data.shape).astype(np.uint8)
+    clustered_result = Image.fromarray(clustered_image)
+    clustered_result.save(output_path)
+
 
 @app.get("/artist_signup", response_class=HTMLResponse)
 async def artist_signup(request: Request):
@@ -54,8 +97,10 @@ async def artist_detail(request: Request):
 
 @app.get("/ai_recomm", response_class=HTMLResponse)
 async def ai_recomm(request: Request):
-    fin = ["추천 단어1", "추천 단어2", "추천 단어3"]
+    emotions = ["행복", "기쁨", "사랑", "세련됨", "감각적", "호기심", "경외심", "슬픔", "미움", "걱정", "혼란", "공포", "노여움", "욕심", "동정"]
+    fin = sample(emotions, 3) 
     return templates.TemplateResponse("ai_recomm.html", {"request": request, "fin": fin})
+
 
 @app.get("/interior_detail_final", response_class=HTMLResponse)
 async def interior_detail_final(request: Request):
